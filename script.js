a33f70294a54e0a8fb1434b960f0001e4438b2ac
Vinicius Vasconcelos <158772638+AgenteRagnarok@users.noreply.github.com>
2024-05-01 15:43:23 -0300

    teste

---
+++ script.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    title: "Seja Bem-Vindo(a)!!",
+    text: "Modal with a custom image.",
+    imageUrl: "https://unsplash.it/400/200",
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: "Custom image"
+});
